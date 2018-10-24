7dbf93e790f50a8ccaaafdc8cca7e8d39e29d9ff
JBMurray6 <34817689+JBMurray6@users.noreply.github.com>
2018-10-24 12:51:15 -0400

    Documentation

--- SerialToAD7124-8.ino
+++ SerialToAD7124-8.ino
@@ -1,27 +1,3 @@
-/*
-  Serial Call and Response in ASCII
-  Language: Wiring/Arduino
-
-  This program sends an ASCII A (byte of value 65) on startup and repeats that
-  until it gets some data in. Then it waits for a byte in the serial port, and
-  sends three ASCII-encoded, comma-separated sensor values, truncated by a
-  linefeed and carriage return, whenever it gets a byte in.
-
-  The circuit:
-  - potentiometers attached to analog inputs 0 and 1
-  - pushbutton attached to digital I/O 2
-
-  created 26 Sep 2005
-  by Tom Igoe
-  modified 24 Apr 2012
-  by Tom Igoe and Scott Fitzgerald
-  Thanks to Greg Shakar and Scott Fitzgerald for the improvements
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/SerialCallResponseASCII
-*/
-
 // include the SPI library:
 #include <SPI.h>
 #include "AD7124-8.h"
